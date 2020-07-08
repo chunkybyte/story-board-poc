5038df2855b78002503f0165fc5dc7098d5788dd
ekanshk <ekansh.k@media.net>
2020-07-08 10:58:07 +0530

    Stories edit and saved in mongo db Complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -26,13 +27,25 @@ const app = express();
 app.use(express.urlencoded( { extended: false } ));
 app.use(express.json());
 
+// Method Override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method;
+            delete req.body._method;
+            return method;
+        }
+    })
+);
+
 // Logging
 if (process.env.NODE_ENV) {
     app.use(morgan('dev'));
 }
 
 // Handlerbars Helpers
-const { formatDate, truncate, stripTags, editIcon } = require('./helpers/hbs');
+const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/hbs');
 const { nextTick } = require('process');
 
 // Handlebars
@@ -41,7 +54,8 @@ app.engine('.hbs', exphbs({
         formatDate,
         truncate,
         stripTags,
-        editIcon
+        editIcon,
+        select
     },
     defaultLayout: 'main', 
     extname: '.hbs'}));
--- routes/stories.js
+++ routes/stories.js
@@ -59,4 +59,72 @@ router.get(
     }
 );
 
+/**
+ * @description Edit a Story Page
+ * @listens     GET /stories/edit/:id
+ */
+router.get(
+    '/edit/:id',
+    ensureAuth,
+    async (req, res)  => {
+        try {
+            const story = await Story.findOne({ 
+                _id: req.params.id
+            }).lean();
+
+            if (!story) {
+                return res.render('errors/404');
+            }
+
+            if (story.user != req.user.id) {
+                res.redirect('/stories');
+            } else {
+                res.render('stories/edit', {
+                    story
+                });
+            }
+            
+        } catch (err) {
+            console.log(err);
+            res.render('errors/500');
+        }
+
+    }
+);
+
+
+/**
+ * @description Update Story Page
+ * @listens     PUT /stories/:id
+ */
+router.put(
+    '/:id',
+    ensureAuth,
+    async (req, res)  => {
+        try {
+            let story = await Story.findById(req.params.id).lean();
+
+            if (!story) {
+                return res.render('errors/404');
+            }
+
+            if (story.user != req.user.id) {
+                res.redirect('/stories');
+            } else {
+                story = await Story.findOneAndUpdate( { _id: req.params.id }, req.body, {
+                    new: true,
+                    runValidators: true
+                });
+
+                res.redirect('/dashboard');
+            }
+
+        } catch (err) {
+            console.log(err)
+            res.render('errors/500'); 
+        }
+        
+    }
+);
+
 module.exports = router;
